60164218d445bee906c8d1a39ddcebfd25a0872a
พศิน พิมพ์คำไหล <dekchairukna@MacBookAir.local>
2026-05-06 20:43:21 +0700

    fix performance and railway startup

--- app.py
+++ app.py
@@ -538,7 +538,7 @@ def index():
             )
         )
         .order_by(Event.date.asc().nullslast(), Event.id.desc())
-        .limit(30)
+        .limit(20)
         .all()
     )
 
@@ -550,7 +550,7 @@ def index():
             latest_match_status_subq.c.unlocked_count == 0
         )
         .order_by(Event.date.desc().nullslast(), Event.id.desc())
-        .limit(40)
+        .limit(20)
         .all()
     )
 
